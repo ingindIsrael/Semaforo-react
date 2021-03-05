4509bba4f7f0de036f44fad707130a04cc13897a
Israel Vasquez <ing.visrael@gmail.com>
2021-03-05 16:45:12 +0000

    remake w/just 1 useState

--- src/js/component/semaforo.js
+++ src/js/component/semaforo.js
@@ -2,48 +2,51 @@ import React from "react";
 import { useState, useEffect } from "react";
 
 export const Semaforo = props => {
-	const [color, setColor] = useState("");
-	const [rojo, setRojo] = useState("negro");
-	const [amarillo, setAmarillo] = useState("negro");
-	const [verde, setVerde] = useState("negro");
-
-	useEffect(() => {
-		if (color === "red") {
-			setAmarillo("negro");
-			setVerde("negro");
-			setRojo(color);
-		} else if (color === "yellow") {
-			setRojo("negro");
-			setAmarillo(color);
-			setVerde("negro");
-		} else if (color === "green") {
-			setRojo("negro");
-			setAmarillo("negro");
-			setVerde(color);
-		}
-	}, [color]);
+	const [color, setColor] = useState({
+		rojo: "negro",
+		amarillo: "negro",
+		verde: "negro"
+	});
 	return (
 		<div>
 			<div id="trafficTop"></div>
 			<div id="container">
 				<div
 					id="light"
-					className={rojo}
-					onClick={() => setColor("red")}>
+					className={color.rojo}
+					onClick={() =>
+						setColor({
+							rojo: "red",
+							amarillo: "negro",
+							verde: "negro"
+						})
+					}>
 					RED
 				</div>
 
 				<div
 					id="light"
-					className={amarillo}
-					onClick={() => setColor("yellow")}>
+					className={color.amarillo}
+					onClick={() =>
+						setColor({
+							amarillo: "yellow",
+							verde: "negro",
+							rojo: "negro"
+						})
+					}>
 					YELLOW
 				</div>
 
 				<div
 					id="light"
-					className={verde}
-					onClick={() => setColor("green")}>
+					className={color.verde}
+					onClick={() =>
+						setColor({
+							verde: "green",
+							amarillo: "negro",
+							rojo: "negro"
+						})
+					}>
 					GREEN
 				</div>
 			</div>
